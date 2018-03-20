4afb378ba7ac0084980de4fff6997439011dfbaf
rhkina <rhkina@gmail.com>
2018-03-20 10:53:55 -0300

    Added server folder and local node modules nodemon and eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "parserOptions": {
+      "ecmaVersion": 6
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off"
+    }
+};

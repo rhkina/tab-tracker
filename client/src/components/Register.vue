1f259e4bf7e929bcd01198aaa814da1e05666e4d
rhkina <rhkina@gmail.com>
2018-03-21 13:44:15 -0300

    Routes configuration

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -28,11 +28,10 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
+      await AuthenticationService.register({
         email: this.email,
         password: this.password
       })
-      console.log(response.data)
     }
   },
   mounted () {
